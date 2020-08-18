4accd585836f9d97ac97b39819ffc1fb1ae5b537
DESKTOP-QSP5N3H\배재홍 <zhfxmtkachdt@daum.net>
2020-08-19 03:38:15 +0900

    Woo1_1, Woo1_2 added

---
+++ src/Woo1_1.java
@@ -0,0 +1,46 @@
+public class Woo1_1 {
+    // 12시 30분 시작. 12시 47분 완료.
+    // 5만원, 만원, 오천원, 천원, 오백원, 백원, 오십원, 십원, 일원
+    // 각각 몇 개로 변환되는지 금액이 큰 순서대로 배열에 담아서 return
+    int w50000 = 50000;
+    int w10000 = 10000;
+    int w5000 = 5000;
+    int w1000 = 1000;
+    int w500 = 500;
+    int w100 = 100;
+    int w50 = 50;
+    int w10 = 10;
+    int w1 = 1;
+
+    public int[] solution(int money) {
+        int[] answer = {};
+        int remainMoneyBy50000 = money % w50000;
+        int remainMoneyBy10000 = remainMoneyBy50000 % w10000;
+        int remainMoneyBy5000 = remainMoneyBy10000 % w5000;
+        int remainMoneyBy1000 = remainMoneyBy5000 % w1000;
+        int remainMoneyBy500 = remainMoneyBy1000 % w500;
+        int remainMoneyBy100 = remainMoneyBy500 % w100;
+        int remainMoneyBy50 = remainMoneyBy100 % w50;
+        int remainMoneyBy10 = remainMoneyBy50 % w10;
+        int remainMoneyBy1 = remainMoneyBy10 / w1;
+        answer = new int[9];
+        answer[0] = money / 50000;
+        answer[1] = remainMoneyBy50000 / 10000;
+        answer[2] = remainMoneyBy10000 / 5000;
+        answer[3] = remainMoneyBy5000 / 1000;
+        answer[4] = remainMoneyBy1000 / 500;
+        answer[5] = remainMoneyBy500 / 100;
+        answer[6] = remainMoneyBy100 / 50;
+        answer[7] = remainMoneyBy50 / 10;
+        answer[8] = remainMoneyBy10 / 1;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Woo1_1 woo1_1 = new Woo1_1();
+        int[] answer = woo1_1.solution(15000);
+        for (int i = 0; i < answer.length; i++) {
+            System.out.println(answer[i]);
+        }
+    }
+}
---
+++ src/Woo1_2.java
@@ -0,0 +1,98 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Woo1_2 {
+    // 12시 48분 시작. 02시 00분 완료
+    int[][] lands;
+    int[][] wells;
+
+    public boolean solution(int[] point) {
+        boolean result = false;
+
+        // lands = new int[][] {{10, 0, 30, 5}, {0, 30, 20, 50}, {30, 30, 40, 40}};
+        // wells = new int[][] {{15, 15, 25, 25}, {40, 10, 50, 20}};
+        // point = new int[] {10, 10, 30, 30};
+
+        lands = new int[][] {{0, 0, 20, 10}, {10, 20, 20, 40}, {30, 0, 50, 20}};
+        wells = new int[][] {{20, 40, 30, 50}, {30, 20, 50, 30}};
+        point = new int[] {20, 30, 30, 40};
+
+        List<Integer> pointX = new ArrayList<Integer>();
+        List<Integer> pointY = new ArrayList<Integer>();
+        pointX.add(point[0]);
+        pointX.add(point[2]);
+        pointY.add(point[1]);
+        pointY.add(point[3]);
+        Collections.sort(pointX);
+        Collections.sort(pointY);
+
+        for (int i = 0; i < lands.length; i++) {
+            List<Integer> landsX = new ArrayList<Integer>();
+            List<Integer> landsY = new ArrayList<Integer>();
+            landsX.add(lands[i][0]);
+            landsX.add(lands[i][2]);
+            landsY.add(lands[i][1]);
+            landsY.add(lands[i][3]);
+            Collections.sort(landsX);
+            Collections.sort(landsY);
+            // System.out.println(pointX.get(0) + " " + pointX.get(1));
+            // System.out.println(pointY.get(0) + " " + pointY.get(1));
+            // System.out.println(landsX.get(0) + " " + landsX.get(1));
+            // System.out.println(landsY.get(0) + " " + landsY.get(1));
+            if (pointX.get(1) <= landsX.get(0) || pointX.get(0) >= landsX.get(1)) {
+                result = true;
+            } else {
+                if (pointY.get(1) <= landsY.get(0) || pointY.get(0) >= landsY.get(1)) {
+                    result = true;
+                } else {
+                    return false;
+                }
+            }
+
+            if (pointY.get(1) <= landsY.get(0) || pointY.get(0) >= landsY.get(1)) {
+                result = true;
+            } else {
+                if (pointX.get(1) <= landsX.get(0) || pointX.get(0) >= landsX.get(1)) {
+                    result = true;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        for (int i = 0; i < wells.length; i++) {
+
+            List<Integer> wellsX = new ArrayList<Integer>();
+            List<Integer> wellsY = new ArrayList<Integer>();
+            wellsX.add(wells[i][0]);
+            wellsX.add(wells[i][2]);
+            wellsY.add(wells[i][1]);
+            wellsY.add(wells[i][3]);
+
+            Collections.sort(wellsX);
+            Collections.sort(wellsY);
+
+            if (pointX.get(1) <= wellsX.get(0) || pointX.get(0) >= wellsX.get(1)) {
+                if (pointY.get(1) <= wellsY.get(0) || pointY.get(0) >= wellsY.get(1)) {
+                    return false;
+                } else {
+                    result = true;
+                }
+            }
+            if (pointY.get(1) <= wellsY.get(0) || pointY.get(0) >= wellsY.get(1)) {
+                if (pointX.get(1) <= wellsX.get(0) || pointX.get(0) >= wellsX.get(1)) {
+                    return false;
+                } else {
+                    result = true;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Woo1_2 woo1_2 = new Woo1_2();
+        System.out.println(woo1_2.solution(new int[] {10, 10, 30, 30}));
+    }
+}
