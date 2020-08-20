969fb71f5473cfbd6e7bf8edf9fd36f514f6fd89
DESKTOP-QSP5N3H\배재홍 <zhfxmtkachdt@daum.net>
2020-08-21 00:48:32 +0900

    Woo1_1Test, Woo1_4Test @CsvSource applied

--- test/Woo1_1Test.java
+++ test/Woo1_1Test.java
@@ -1,17 +1,19 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class Woo1_1Test {
 
     @ParameterizedTest
-    @ValueSource(strings = {"050237", "115000"})
-    public void test(String moneyStr) {
+    @CsvSource(value = {"50237:[1, 0, 0, 0, 0, 2, 0, 3, 7]", "15000:[0, 1, 1, 0, 0, 0, 0, 0, 0]"}, delimiter = ':')
+    public void test(String money, String expectedStr) {
         Woo1_1 woo1_1 = new Woo1_1();
-        int index = Integer.parseInt(moneyStr.substring(0, 1));
-        int money = Integer.parseInt(moneyStr.substring(1));
-        int[] answer = woo1_1.solution(money);
-        int[][] correctAnswer = {{1, 0, 0, 0, 0, 2, 0, 3, 7}, {0, 1, 1, 0, 0, 0, 0, 0, 0}};
-        Assertions.assertArrayEquals(correctAnswer[index], answer);
+        String[] expectedStrArray = expectedStr.replace("[", "").replace("]", "").split(", ");
+        int[] expected = new int[expectedStrArray.length];
+        for (int i = 0; i < expected.length; i++) {
+            expected[i] = Integer.parseInt(expectedStrArray[i]);
+        }
+        int[] answer = woo1_1.solution(Integer.parseInt(money));
+        Assertions.assertArrayEquals(expected, answer);
     }
 }
--- test/Woo1_4Test.java
+++ test/Woo1_4Test.java
@@ -1,24 +1,22 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class Woo1_4Test {
 
     @ParameterizedTest
-    @ValueSource(strings = {"097, 98, 197, 198", "1131, 132, 211, 212", "299, 102, 211, 212"})
-    public void test(String testCase) {
-        int index = Integer.parseInt(testCase.substring(0, 1));
-        testCase = testCase.substring(1);
+    @CsvSource(value = {"97, 98, 197, 198:0", "131, 132, 211, 212:1", "99, 102, 211, 212:-1"}, delimiter = ':')
+    public void test(String testCase, String expected) {
         Woo1_4 woo1_4 = new Woo1_4();
+        int expectedAnswer = Integer.parseInt(expected);
         int[] pobi = new int[2];
         int[] crong = new int[2];
-        int[] correctAnswer = new int[] {0, 1, -1};
         pobi[0] = Integer.parseInt(testCase.split(", ")[0]);
         pobi[1] = Integer.parseInt(testCase.split(", ")[1]);
         crong[0] = Integer.parseInt(testCase.split(", ")[2]);
         crong[1] = Integer.parseInt(testCase.split(", ")[3]);
         int answer = woo1_4.solution(pobi, crong);
-        Assertions.assertEquals(correctAnswer[index], answer);
+        Assertions.assertEquals(expectedAnswer, answer);
 
     }
 }
