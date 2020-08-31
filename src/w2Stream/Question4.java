4086d3c55d76b1571218f125167b44119bb032b4
DESKTOP-QSP5N3H\배재홍 <zhfxmtkachdt@daum.net>
2020-08-31 21:13:46 +0900

    Question4 Stream으로 구조변경하였음.

--- src/w2Stream/Question4.java
+++ src/w2Stream/Question4.java
@@ -1,5 +1,7 @@
 package w2Stream;
 
+import java.util.Arrays;
+import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
 // 02시 40분 시작 03시 05분 완료
@@ -9,67 +11,59 @@ public class Question4 {
 
         final boolean[] addCheck = {false};
         final boolean[] loginCheck = {false};
-        // for (int i = 0; i < actions.length; i++) {
-        //     if (actions[i].contains("LOGIN")) {
-        //         if (loginCheck == false) {
-        //             for (int j = 0; j < infos.length; j++) {
-        //                 if (actions[i].contains(infos[j])) {
-        //                     answer[i] = true;
-        //                     loginCheck = true;
-        //                     break;
-        //                 } else {
-        //                     answer[i] = false;
-        //                 }
-        //             }
-        //         } else {
-        //             answer[i] = false;
-        //         }
-        //     }
-        //     if (actions[i].contains("ADD")) {
-        //         if (loginCheck == true) {
-        //             answer[i] = true;
-        //             addCheck = true;
-        //         } else {
-        //             answer[i] = false;
-        //         }
-        //     }
-        //     if (actions[i].contains("ORDER") && addCheck == true) {
-        //         answer[i] = true;
-        //         addCheck = false;
-        //     }
-        // }
+
+        Predicate<String> login = s -> s.contains("LOGIN");
+        Predicate<String> add = s -> s.contains("ADD");
+        Predicate<String> order = s -> s.contains("ORDER");
 
         IntStream.range(0, actions.length).forEach(index -> {
-            if (actions[index].contains("LOGIN")) {
-                if (loginCheck[0] == false) {
-                    for (int j = 0; j < infos.length; j++) {
-                        if (actions[index].contains(infos[j])) {
-                            answer[index] = true;
-                            loginCheck[0] = true;
-                            break;
-                        } else {
-                            answer[index] = false;
-                        }
-                    }
-                }
-                if (loginCheck[0] == true) {
-                    answer[index] = false;
-                }
+            if (login.test(actions[index])) {
+                loginCheck(infos, actions, answer, loginCheck, login, index);
+                return;
             }
-            if (actions[index].contains("ADD")) {
-                if (loginCheck[0] == true) {
-                    answer[index] = true;
-                    addCheck[0] = true;
-                } else {
-                    answer[index] = false;
-                }
+            if (add.test(actions[index])) {
+                addCheck(answer, addCheck, loginCheck, index);
+                return;
             }
-            if (actions[index].contains("ORDER") && addCheck[0] == true) {
-                answer[index] = true;
-                addCheck[0] = false;
+            if (order.test(actions[index]) && addCheck[0] == true) {
+                orderCheck(answer, addCheck, index);
+                return;
             }
         });
 
         return answer;
     }
+
+    private void orderCheck(boolean[] answer, boolean[] addCheck, int index) {
+        if (addCheck[0] == true) {
+            answer[index] = true;
+            addCheck[0] = false;
+        } else {
+            answer[index] = false;
+        }
+        return;
+    }
+
+    private void addCheck(boolean[] answer, boolean[] addCheck, boolean[] loginCheck, int index) {
+        if (loginCheck[0] == true) {
+            answer[index] = true;
+            addCheck[0] = true;
+            return;
+        } else {
+            answer[index] = false;
+        }
+    }
+
+    private void loginCheck(String[] infos, String[] actions, boolean[] answer, boolean[] loginCheck,
+        Predicate<String> login, int index) {
+        if (loginCheck[0] == false) {
+            if (Arrays.stream(infos).anyMatch(info -> actions[index].contains(info))) {
+                answer[index] = true;
+                loginCheck[0] = true;
+            }
+        } else {
+            answer[index] = false;
+        }
+    }
+
 }
