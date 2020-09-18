c90d0533e259fa403b1aa34901b3cbb3495ac240
koitt03a-PC\koitt03a <zhfxmtkachdt@daum.net>
2020-09-18 11:21:16 +0900

    2020/09/18 푼 문제들 추가

---
+++ src/programmers_level1/SumOfTwoNumber.java
@@ -0,0 +1,51 @@
+package programmers_level1;
+
+import java.util.*;
+
+// 두 개 뽑아서 더하기
+//09시 47분 시작 09시 55분 완료.
+public class SumOfTwoNumber {
+    public int[] solution(int[] numbers) {
+        int length = numbers.length;
+//        List<Integer> sums = new ArrayList<>();
+        Set<Integer> set = new TreeSet<>();
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                int sum = numbers[i] + numbers[j];
+//                if (i != j && !sums.contains(sum)) {
+//                    sums.add(sum);
+                if (i != j) {
+                    set.add(sum);
+                }
+//                }
+            }
+        }
+        Map<String, Integer> map = new HashMap<>();
+        map.keySet();
+        Set<Map.Entry<String, Integer>> entry = map.entrySet();
+        Iterator<Integer> iterator = set.iterator();
+        iterator.remove();
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+        System.out.println(iterator.hasNext());
+
+        return set.stream()
+                .mapToInt(Integer::intValue)
+                .sorted()
+                .toArray();
+
+//        return sums.stream()
+//                .mapToInt(Integer::intValue)
+//                .sorted()
+//                .toArray();
+    }
+
+    public static void main(String[] args) {
+        SumOfTwoNumber sumOfTwoNumber = new SumOfTwoNumber();
+        int[] numbers = {2, 1, 3, 4, 1};
+        sumOfTwoNumber.solution(numbers);
+//        Arrays.stream(sumOfTwoNumber.solution(numbers))
+//                .forEach(System.out::println);
+    }
+}
--- src/programmers_level1/TrainingCloth.java
+++ src/programmers_level1/TrainingCloth.java
@@ -1,35 +1,73 @@
 package programmers_level1;
 
-import java.util.stream.IntStream;
+import java.util.HashSet;
+import java.util.Set;
 
-// 18시 23분 시작
+// 18시 23분 시작 실패 측정 못함.
+// 탐욕법에 대해 생각해보자..
 public class TrainingCloth {
     public int solution(int n, int[] lost, int[] reserve) {
-        boolean[] check = new boolean[n];
-        boolean[] reserveCheck = new boolean[n];
-        int count = reserve.length;
+        int answer = n - lost.length;
+
+        Set<Integer> exist = new HashSet<>();
+        for (int i = 0; i < reserve.length; i++) {
+            exist.add(reserve[i]);
+        }
+
         for (int i = 0; i < lost.length; i++) {
-            if (count == 0) {
-                break;
-            }
-            for (int j = 0; j < reserve.length; j++) {
-                if ((lost[i] + 1 == reserve[j]) && !reserveCheck[reserve[j] - 1]) {
-                    check[lost[i] - 1] = true;
-                    reserveCheck[reserve[j] - 1] = true;
-                    count -= 1;
-                    break;
-                }
+            if (exist.contains(lost[i])) {
+                answer += 1;
+                exist.remove(lost[i]);
+                lost[i] = -1;
             }
         }
-        return (int) IntStream.range(0, n)
-            .filter(index -> check[index])
-            .count() + n - lost.length;
+
+        for (int i = 0; i < lost.length; i++) {
+            if (exist.contains(lost[i] - 1)) {
+                answer += 1;
+                exist.remove(lost[i] - 1);
+                continue;
+            }
+            if (exist.contains(lost[i] + 1)) {
+                answer += 1;
+                exist.remove(lost[i] + 1);
+                continue;
+            }
+        }
+
+//        int existCount = 0;
+//        int reservedCount = 0;
+//        //여벌 옷을 가지고 있는 학생이 도난 당하면 빌려줄 수 없도록 만든다.
+//        for (int i = 0; i < lost.length; i++) {
+//            for (int j = 0; j < reserve.length; j++) {
+//                if (lost[i] == reserve[j]) {
+//                    existCount += 1;
+//                    reserve[j] = -1;
+//                    lost[i] = -1;
+//                    break;
+//                }
+//            }
+//        }
+//
+//        //옷을 빌려주고 -1로 만들어 뒤의 학생에게 빌려주지 않게 한다.
+//        for (int i = 0; i < lost.length; i++) {
+//            for (int j = 0; j < reserve.length; j++) {
+//                if (lost[i] == reserve[j] + 1 || lost[i] == reserve[j] - 1) {
+//                    reservedCount++;
+//                    reserve[j] = -1;
+//                    break;
+//                }
+//            }
+//        }
+
+        return answer;
     }
 
     public static void main(String[] args) {
         TrainingCloth trainingCloth = new TrainingCloth();
-        int[] lost = {2, 4};
+        int[] lost = {1, 3, 5};
         int[] reserve = {3};
         System.out.println(trainingCloth.solution(5, lost, reserve));
+
     }
 }
