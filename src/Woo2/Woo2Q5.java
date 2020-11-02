ee545f4433065c6b59f506687b4b6b0b66390794
DESKTOP-QSP5N3H\배재홍 <zhfxmtkachdt@daum.net>
2020-11-02 20:42:07 +0900

    2020/11/02 푼 문제들 추가

--- src/Woo2/Woo2Q5.java
+++ src/Woo2/Woo2Q5.java
@@ -1,4 +1,82 @@
 package Woo2;
 
+import java.util.Arrays;
+
+// 27분 소요.
 public class Woo2Q5 {
+    private static final int REQUIRED_PIG_FOOT = 4;
+    private static final int REQUIRED_ONION = 50;
+    private static final int REQUIRED_GREEN_ONION = 10;
+    private static final int REQUIRED_GARLIC = 10;
+    private static final int REQUIRED_PEPPER = 4;
+    private static final int PIG_FOOT_PRICE = 10000;
+    private static final int ONION_PRICE = 3000;
+    private static final int GREEN_ONION_PRICE = 1000;
+    private static final int GARLIC_PRICE = 2000;
+    private static final int PEPPER_PRICE = 1000;
+
+    public int[] solution(String[] history) {
+        double pigFoot = 5;
+        double onion = 100;
+        double greenOnion = 10;
+        double garlic = 5;
+        double pepper = 2;
+
+        int[] answer = new int[history.length];
+        for (int i = 0; i < history.length; i++) {
+            int price = 0;
+            String[] input = history[i].split("\\.");
+            if (Integer.valueOf(input[0]) != 1 && Integer.valueOf(input[0]) != 2) {
+                return new int[] {-1};
+            }
+            if (Integer.valueOf(input[1]) != 0 && Integer.valueOf(input[1]) != 5) {
+                return new int[] {-1};
+            }
+            double requiredPigFoot = Double.valueOf(history[i]) * REQUIRED_PIG_FOOT;
+            double requiredOnion = Double.valueOf(history[i]) * REQUIRED_ONION;
+            double requiredGreenOnion = Double.valueOf(history[i]) * REQUIRED_GREEN_ONION;
+            double requiredGarlic = Double.valueOf(history[i]) * REQUIRED_GARLIC;
+            double requiredPepper = Double.valueOf(history[i]) * REQUIRED_PEPPER;
+            if (input[1].equals("5")) {
+                requiredPepper /= 2;
+            }
+            while (pigFoot < requiredPigFoot) {
+                pigFoot += 10;
+                price += PIG_FOOT_PRICE;
+            }
+            pigFoot = pigFoot - requiredPigFoot;
+
+            while (onion < requiredOnion) {
+                onion += 100;
+                price += ONION_PRICE;
+            }
+            onion = onion - requiredOnion;
+
+            while (greenOnion < requiredGreenOnion) {
+                greenOnion += 30;
+                price += GREEN_ONION_PRICE;
+            }
+            greenOnion = greenOnion - requiredGreenOnion;
+
+            while (garlic < requiredGarlic) {
+                garlic += 50;
+                price += GARLIC_PRICE;
+            }
+            garlic = garlic - requiredGarlic;
+
+            while (pepper < requiredPepper) {
+                pepper += 10;
+                price += PEPPER_PRICE;
+            }
+            pepper = pepper - requiredPepper;
+            answer[i] = price;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Woo2Q5 woo2Q5 = new Woo2Q5();
+        String[] history = {"1.0", "2.0", "1.5"};
+        System.out.println(Arrays.toString(woo2Q5.solution(history)));
+    }
 }
--- src/Woo2/Woo2Q7.java
+++ src/Woo2/Woo2Q7.java
@@ -1,8 +1,97 @@
 package Woo2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+// 35분 걸림
 public class Woo2Q7 {
     public String[] solution(String user, String[][] friends, String[] visitors) {
-        String[] answer = {};
-        return answer;
+
+        List<String> friendList = new ArrayList<>();
+        for (String[] relation : friends) {
+            boolean check = Arrays.stream(relation)
+                .anyMatch(user::equals);
+            if (check) {
+                Arrays.stream(relation)
+                    .findFirst()
+                    .ifPresent(name -> friendList.add(name));
+            }
+        }
+
+        List<String> friendToFriend = new ArrayList<>();
+        for (String[] relation : friends) {
+            for (String name : friendList) {
+                boolean isFriendToFriend = Arrays.stream(relation)
+                    .anyMatch(name::equals);
+                if (isFriendToFriend) {
+                    Arrays.stream(relation)
+                        .filter(item -> !item.equals(name))
+                        .filter(item -> !item.equals(user))
+                        .findFirst()
+                        .ifPresent(item -> friendToFriend.add(item));
+                }
+            }
+        }
+
+        Map<String, Integer> recommends = new HashMap<>();
+        for (String name : friendToFriend) {
+            if (!recommends.containsKey(name)) {
+                recommends.put(name, 10);
+                continue;
+            }
+            recommends.put(name, recommends.get(name) + 10);
+        }
+
+        for (String name : visitors) {
+            if (!recommends.containsKey(name)) {
+                recommends.put(name, 1);
+                continue;
+            }
+            recommends.put(name, recommends.get(name) + 1);
+        }
+
+        List<User> users = new ArrayList<>();
+        for (Map.Entry<String, Integer> recommend : recommends.entrySet()) {
+            users.add(new User(recommend.getKey(), recommend.getValue()));
+        }
+
+        return users.stream()
+            .filter(info -> info.score != 0)
+            .filter(info -> !friendList.contains(info.name))
+            .sorted(new Comparator<User>() {
+                @Override
+                public int compare(User o1, User o2) {
+                    if (o1.score == o2.score) {
+                        return o1.name.compareTo(o2.name);
+                    }
+                    return -(o1.score - o2.score);
+                }
+            })
+            .map(info -> info.name)
+            .limit(5)
+            .toArray(String[]::new);
+    }
+
+    class User {
+        private String name;
+        private int score;
+
+        public User(String name, int score) {
+            this.name = name;
+            this.score = score;
+        }
+    }
+
+    public static void main(String[] args) {
+        String user = "mrko";
+        String[][] friends = {{"donut", "andole"}, {"donut", "jun"}, {"donut", "mrko"}, {"shakevan", "andole"},
+            {"shakevan", "jun"}, {"shakevan", "mrko"}, {"jun", "mrko"}, {"bedi", "jun"}};
+        String[] visitors = {"bedi", "bedi", "donut", "bedi", "shakevan", "ji", "ji", "ji", "ji"};
+        Woo2Q7 woo2Q7 = new Woo2Q7();
+        System.out.println(Arrays.toString(woo2Q7.solution(user, friends, visitors)));
     }
 }
